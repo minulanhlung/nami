46efd6fb906322dd6e69322693168b59a0dfb1e1
minulanhlung <163533971+minulanhlung@users.noreply.github.com>
2024-03-16 22:35:20 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -11,4 +11,67 @@ window.addEventListener("scroll", () => {
     }else{
         totop.classList.remove("active");
     }
-})
+});
+
+const form = document.querySelector('form');
+const fullname = document.getElementById("name");
+const email = document.getElementById("mail");
+const message = document.getElementById("mess");
+const phone = document.getElementById("phone");
+
+function sendEmail(){
+    const bodymessage = `Name: ${fullname.value}<br> Email: ${email.value}
+    <br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
+
+
+    Email.send({
+        // SecureToken:
+        Host : "smtp.elasticemail.com",
+        Username : "methuy115@gmail.com",
+        Password : "B87124840C284FC7423B6EE1B1CAE969674A",
+        To : 'methuy115@gmail.com',
+        From : email.value,
+        Subject : "Feedback from customers",
+        Body : bodymessage
+    }).then(
+      message => {
+        if(message == "OK"){
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Thanks Your Feedback",
+                showConfirmButton: false,
+                timer: 2000
+              });
+        }
+      }
+    );
+}
+
+function checkinputs(){
+    const input = document.querySelectorAll(".contact-input");
+
+    for (const item of input ){
+        if(item.value == ""){
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+    item.addEventListener("keyup", () => {
+        if(item.value != ""){
+            item.classList.remove("error");
+            item.parentElement.classList.remove("error");
+        }
+        else{
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+    })
+    }
+}
+
+form.addEventListener("submit", (e) => { 
+      e.preventDefault();
+      sendEmail();
+});
+
